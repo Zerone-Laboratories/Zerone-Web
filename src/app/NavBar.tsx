94178115b7b5dd3b39ecdeab5059a57eaabd9940
AyoItsYas <yasiru.dharmathilaka@gmail.com>
2023-12-23 18:16:39 +0530

    Add scroll effect to NavBar and update styling

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,6 +1,28 @@
+"use client";
+
 import styles from "./NavBar.module.scss";
 
 function NavBar({ children }: { children: React.ReactNode }) {
+  function handleReady() {
+    while (document.readyState !== "complete") {}
+
+    const navBar = document.querySelector(`.${styles.NavBar}`) as HTMLElement;
+
+    function handleScroll() {
+      if (window.scrollY > 0) {
+        navBar.classList.add(styles.NavBarScrolled);
+      } else {
+        navBar.classList.remove(styles.NavBarScrolled);
+      }
+    }
+
+    if (navBar) {
+      window.addEventListener("scroll", handleScroll);
+    }
+  }
+
+  window.addEventListener("load", handleReady);
+
   return <nav className={styles.NavBar}>{children}</nav>;
 }
 
